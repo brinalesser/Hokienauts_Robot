8ff3cbfedf2667152061ea07afb070595dde48de
Sabrina Lesser <brinalesser@vt.edu>
2022-02-06 15:17:15 -0500

    fixed print

--- btServer.py
+++ btServer.py
@@ -4,11 +4,11 @@ from btpycom import *
 
 def onStateChanged(state, msg):
     if state == "LISTENING":
-        print "Server is listening"
+        print("Server is listening")
     elif state == "CONNECTED":
-        print "Connection established to", msg
+        print("Connection established to", msg)
     elif state == "MESSAGE":
-        print "Got message", msg
+        print("Got message", msg)
         server.sendMessage(msg)
 
 serviceName = "EchoServer"
