37218cabf76c6828812fdd5ef01d59c643c0d85c
Sabrina Lesser <brinalesser@vt.edu>
2022-02-06 17:33:12 -0500

    new example

--- btController.py
+++ btController.py
@@ -1,49 +1,34 @@
-#!/usr/bin/env python3
-"""PyBluez simple example rfcomm-client.py
-Simple demonstration of a client application that uses RFCOMM sockets intended
-for use with rfcomm-server.
-Author: Albert Huang <albert@csail.mit.edu>
-$Id: rfcomm-client.py 424 2006-08-24 03:35:54Z albert $
-"""
+# BtEchoClient.py
 
-import sys
+from btcom import *  # TigerJython
+from btpycom import *  # Standard-Python (PC, Raspi, ...)
 
-import bluetooth
+def onStateChanged(state, msg):
+    global reply
+    if state == "CONNECTING":
+        print "Connecting", msg
+    elif state == "CONNECTION_FAILED":
+        print "Connection failed", msg
+    elif state == "CONNECTED":
+        print "Connected", msg
+    elif state == "DISCONNECTED":
+        print "Disconnected", msg
+    elif state == "MESSAGE":
+        print "Message", msg
+        reply = msg
 
-
-addr = None
-
-if len(sys.argv) < 2:
-    print("No device specified. Searching all nearby bluetooth devices for "
-          "the SampleServer service...")
+serviceName = "EchoServer"
+print "Performing search for service name", serviceName
+client = BTClient(stateChanged = onStateChanged)
+serverInfo = client.findService(serviceName, 20)
+if serverInfo == None:
+    print "Service search failed"
 else:
-    addr = sys.argv[1]
-    print("Searching for SampleServer on {}...".format(addr))
-
-# search for the SampleServer service
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-service_matches = bluetooth.find_service(uuid=uuid, address=addr)
-
-if len(service_matches) == 0:
-    print("Couldn't find the SampleServer service.")
-    sys.exit(0)
-
-first_match = service_matches[0]
-port = first_match["port"]
-name = first_match["name"]
-host = first_match["host"]
-
-print("Connecting to \"{}\" on {}".format(name, host))
-
-# Create the client socket
-sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-sock.connect((host, port))
-
-print("Connected. Type something...")
-while True:
-    data = input()
-    if not data:
-        break
-    sock.send(data)
-
-sock.close()
+    print "Got server info", serverInfo
+    if client.connect(serverInfo, 20):
+        for n in range(0, 101):
+            client.sendMessage(str(n))
+            reply = ""
+            while reply == "":
+                time.sleep(0.001)
+        client.disconnect()  
