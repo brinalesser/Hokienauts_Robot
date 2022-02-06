ffb70dc928d5703cf4fd765799746c3af90e8893
Sabrina Lesser <brinalesser@vt.edu>
2022-02-06 18:01:56 -0500

    new example

--- btController.py
+++ btController.py
@@ -1,34 +1,32 @@
-# BtEchoClient.py
+import bluetooth
 
-from btcom import *  # TigerJython
-from btpycom import *  # Standard-Python (PC, Raspi, ...)
+def receiveMessages():
+  server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-def onStateChanged(state, msg):
-    global reply
-    if state == "CONNECTING":
-        print("Connecting", msg)
-    elif state == "CONNECTION_FAILED":
-        print("Connection failed", msg)
-    elif state == "CONNECTED":
-        print("Connected", msg)
-    elif state == "DISCONNECTED":
-        print("Disconnected", msg)
-    elif state == "MESSAGE":
-        print("Message", msg)
-        reply = msg
+  port = 1
+  server_sock.bind(("",port))
+  server_sock.listen(1)
 
-serviceName = "EchoServer"
-print("Performing search for service name", serviceName)
-client = BTClient(stateChanged = onStateChanged)
-serverInfo = client.findService(serviceName, 20)
-if serverInfo == None:
-    print("Service search failed")
-else:
-    print("Got server info", serverInfo)
-    if client.connect(serverInfo, 20):
-        for n in range(0, 101):
-            client.sendMessage(str(n))
-            reply = ""
-            while reply == "":
-                time.sleep(0.001)
-        client.disconnect()
+  client_sock,address = server_sock.accept()
+  print("Accepted connection from " + str(address))
+
+  data = client_sock.recv(1024)
+  print("received [%s]" % data)
+
+  client_sock.close()
+  server_sock.close()
+
+def sendMessageTo(targetBluetoothMacAddress):
+  port = 1
+  sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  sock.connect((targetBluetoothMacAddress, port))
+  sock.send("hello!!")
+  sock.close()
+
+def lookUpNearbyBluetoothDevices():
+  nearby_devices = bluetooth.discover_devices()
+  for bdaddr in nearby_devices:
+    print(str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]")
+
+
+lookUpNearbyBluetoothDevices()
--- btServer.py
+++ btServer.py
@@ -1,15 +1,32 @@
-# BtEchoServer.py
+import bluetooth
 
-from btpycom import *
+def receiveMessages():
+  server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-def onStateChanged(state, msg):
-    if state == "LISTENING":
-        print("Server is listening")
-    elif state == "CONNECTED":
-        print("Connection established to", msg)
-    elif state == "MESSAGE":
-        print("Got message", msg)
-        server.sendMessage(msg)
+  port = 1
+  server_sock.bind(("",port))
+  server_sock.listen(1)
 
-serviceName = "EchoServer"
-server = BTServer(serviceName, stateChanged = onStateChanged)
+  client_sock,address = server_sock.accept()
+  print("Accepted connection from " + str(address))
+
+  data = client_sock.recv(1024)
+  print("received [%s]" % data)
+
+  client_sock.close()
+  server_sock.close()
+
+def sendMessageTo(targetBluetoothMacAddress):
+  port = 1
+  sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  sock.connect((targetBluetoothMacAddress, port))
+  sock.send("hello!!")
+  sock.close()
+
+def lookUpNearbyBluetoothDevices():
+  nearby_devices = bluetooth.discover_devices()
+  for bdaddr in nearby_devices:
+    print(str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]")
+
+
+lookUpNearbyBluetoothDevices()
