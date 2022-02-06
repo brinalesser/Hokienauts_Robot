18c33c56130731f7e97a2adb1a168b6bd7d8158e
Sabrina Lesser <brinalesser@vt.edu>
2022-02-06 13:43:42 -0500

    Added Bluetooth Test Code

---
+++ btServer.py
@@ -0,0 +1,15 @@
+# BtEchoServer.py
+
+from btpycom import *
+
+def onStateChanged(state, msg):
+    if state == "LISTENING":
+        print "Server is listening"
+    elif state == "CONNECTED":
+        print "Connection established to", msg
+    elif state == "MESSAGE":
+        print "Got message", msg
+        server.sendMessage(msg)
+
+serviceName = "EchoServer"
+server = BTServer(serviceName, stateChanged = onStateChanged)
