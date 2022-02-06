7c05d1ae9af6ed5a3c70175f2a60d39d682b62ea
Sabrina Lesser <brinalesser@vt.edu>
2022-02-06 17:24:26 -0500

    new example

--- btServer.py
+++ btServer.py
@@ -1,16 +1,41 @@
+"""PyBluez simple example rfcomm-server.py
+Simple demonstration of a server application that uses RFCOMM sockets.
+Author: Albert Huang <albert@csail.mit.edu>
+$Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
+"""
+
 import bluetooth
 
-server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-
-port = 8000
-server_sock.bind(("",port))
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", bluetooth.PORT_ANY))
 server_sock.listen(1)
 
-client_sock,address = server_sock.accept()
-print("Accepted connection from ",address)
+port = server_sock.getsockname()[1]
 
-data = client_sock.recv(1024)
-print("received [%s]" % data)
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                            # protocols=[bluetooth.OBEX_UUID]
+                            )
+
+print("Waiting for connection on RFCOMM channel", port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if not data:
+            break
+        print("Received", data)
+except OSError:
+    pass
+
+print("Disconnected.")
 
 client_sock.close()
 server_sock.close()
+print("All done.")
