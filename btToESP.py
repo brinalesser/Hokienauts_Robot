b560f3bedebb5a1c0d0f126f5d48092dfa5f887d
brinalesser <49353127+brinalesser@users.noreply.github.com>
2022-02-20 16:49:40 -0500

    Update btToESP.py

--- btToESP.py
+++ btToESP.py
@@ -1,53 +1,41 @@
 from bluetooth import *
 
-def input_and_send():
-    print("\nType something\n")
-    while True:
-        data = input()
-        if len(data) == 0: break
-        sock.send(data)
-        sock.send("\n")
+def send_msg():
+    print("\nInput message to send:\n")
+    data = input()
+    sock.send(data)
         
-def rx_and_echo():
-    sock.send("\nsend anything\n")
-    while True:
-        data = sock.recv(buf_size)
-        if data:
-            print(data)
-            sock.send(data)
+def receive_msg():
+    data = sock.recv(buf_size)
+    if data:
+        print("Received: [%s]",data)
             
 #MAC address of ESP32
-addr = "40:91:51:9B:E3:AC"
-#uuid = "3cf7e481-e72b-4c2d-b841-f57930fd3d50"
-#service_matches = find_service( uuid = uuid, address = addr )
+addr = "40:91:51:9B:E3:AE"
 service_matches = find_service( address = addr )
 
 buf_size = 1024;
 
 if len(service_matches) == 0:
-    print("couldn't find the SampleServer service =(")
+    print("Unable to find the ESP32 Device")
     sys.exit(0)
 
-for s in range(len(service_matches)):
-    print("\nservice_matches: [" + str(s) + "]:")
-    print(service_matches[s])
+print("\nService:")
+print(service_matches[0])
     
-first_match = service_matches[0]
-port = first_match["port"]
-name = first_match["name"]
-host = first_match["host"]
+port = service_matches[0]["port"]
+name = service_matches[0]["name"]
+host = service_matches[0]["host"]
 
-port=1
-print("connecting to \"%s\" on %s, port %s" % (name, host, port))
+print("Connecting to \"%s\" on host %s and port %s" % (name, host, port))
 
 # Create the client socket
 sock=BluetoothSocket(RFCOMM)
 sock.connect((host, port))
 
-print("connected")
+print("Connected")
 
-#input_and_send()
-rx_and_echo()
+send_msg()
+receive_msg()
 
 sock.close()
-print("\n--- bye ---\n")
