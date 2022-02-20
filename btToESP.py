6a7eff742f11a1ad79f420648ae8c607f837c8a6
brinalesser <49353127+brinalesser@users.noreply.github.com>
2022-02-20 17:18:07 -0500

    Update btToESP.py

--- btToESP.py
+++ btToESP.py
@@ -1,20 +1,93 @@
 from bluetooth import *
+import move
+
+speed_set = 100
+rad = 0.5
+buf_size = 1024;
+
+#MAC address of ESP32
+addr = "40:91:51:9B:E3:AE"
 
 def send_msg():
-    print("\nInput message to send:\n")
+    print("\nSend a message to initiate motor control:\n")
     data = input()
     sock.send(data)
         
-def receive_msg():
-    data = sock.recv(buf_size)
-    if data:
-        print(data)
-            
-#MAC address of ESP32
-addr = "40:91:51:9B:E3:AE"
-service_matches = find_service( address = addr )
+def robotCtrl(command_input):
+    global direction_command, turn_command
+    if 'forward' == command_input:
+        direction_command = 'forward'
+        move.move(speed_set, 'forward', 'no', rad)
+    
+    elif 'backward' == command_input:
+        direction_command = 'backward'
+        move.move(speed_set, 'backward', 'no', rad)
 
-buf_size = 1024;
+    elif 'DS' in command_input:
+        direction_command = 'no'
+        if turn_command == 'no':
+            move.move(speed_set, 'no', 'no', rad)
+
+    elif 'left' == command_input:
+        turn_command = 'left'
+        move.move(speed_set, 'no', 'left', rad)
+
+    elif 'right' == command_input:
+        turn_command = 'right'
+        move.move(speed_set, 'no', 'right', rad)
+
+    elif 'TS' in command_input:
+        turn_command = 'no'
+        if direction_command == 'no':
+            move.move(speed_set, 'no', 'no', rad)
+        else:
+            move.move(speed_set, direction_command, 'no', rad)
+
+def receive_msg():
+    
+    move.setup()
+    direction_command = 'no'
+    turn_command = 'no'
+
+    while True: 
+        response = {
+            'status' : 'ok',
+            'title' : '',
+            'data' : None
+        }
+
+        data = ''
+        data = sock.recv(buf_size).decode()
+        
+        try:
+            data = json.loads(data)
+            
+        except Exception as e:
+            print('not A JSON')
+            return False
+
+        if isinstance(data,str):
+            robotCtrl(data, response)
+
+            if 'get_info' == data:
+                response['title'] = 'get_info'
+                response['data'] = ['info']
+
+            if 'wsB' in data:
+                try:
+                    set_B=data.split()
+                    speed_set = int(set_B[1])
+                except:
+                    pass
+        else:
+            pass
+
+        print(data)
+        response = json.dumps(response)
+        await websocket.send(response)
+            
+#connect to ESP32
+service_matches = find_service( address = addr )
 
 if len(service_matches) == 0:
     print("Unable to find the ESP32 Device")
@@ -36,6 +109,8 @@ sock.connect((host, port))
 print("Connected")
 
 send_msg()
-receive_msg()
+
+while(receive_msg):
+    continue
 
 sock.close()
