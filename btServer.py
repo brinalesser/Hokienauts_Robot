536b60b16a6ab5e8b7ae7b929257f594adc259ed
brinalesser <49353127+brinalesser@users.noreply.github.com>
2022-02-20 15:53:33 -0500

    Update btServer.py

--- btServer.py
+++ btServer.py
@@ -2,15 +2,37 @@ import bluetooth
 
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-port = 1
-server_sock.bind(("",port))
+server_sock.bind(("",PORT_ANY))
 server_sock.listen(1)
+port = server_sock.getsockname()[1]
 
-client_sock,address = server_sock.accept()
-print("Accepted connection from ",address)
+uuid = "7179855a-3507-4dd4-a2ad-c0ff1175fdcd"
 
-data = client_sock.recv(1024)
-print("received [%s]" % data)
+advertise_service( server_sock, "HokienautsPiServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ] )
 
-client_sock.close()
-server_sock.close()
+while True:          
+    print("Waiting for connection on RFCOMM channel %d" % port)
+
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from ", client_info)
+
+    try:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        print("received [%s]" % data)
+        data = 'Hello!' 
+        client_sock.send(data)
+        print("sending [%s]" % data)
+
+    except IOError:
+        pass
+
+    except KeyboardInterrupt:
+        print("disconnected")
+        client_sock.close()
+        server_sock.close()
+        print("all done")
+        break
