4f597cc0a17e75154bfa547fd6dd9c052b83d2f7
Sabrina Lesser <brinalesser@vt.edu>
2022-03-16 00:43:36 -0400

    added unity code

--- ESP32_Bluetooth/ESP32_Bluetooth.ino
+++ ESP32_Bluetooth/ESP32_Bluetooth.ino
@@ -1,47 +1,39 @@
-// ref: Examples > BluetoothSerial > SerialToSerialBT
-
 #include "BluetoothSerial.h"
-#include "esp_bt_device.h"
-
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
+    
 BluetoothSerial SerialBT;
 
-void printDeviceAddress() {
-  const uint8_t* point = esp_bt_dev_get_address();
-  for (int i = 0; i < 6; i++) {
-    char str[3];
-    sprintf(str, "%02X", (int)point[i]);
-    Serial.print(str);
-    if (i < 5){
-      Serial.print(":");
-    }
-  }
-}
-
-void setup() {
+String btInput;
+String serialInput;
+     
+void setup()
+{
   Serial.begin(115200);
-  
-  if(SerialBT.begin("HokienautsESP32")){
-    Serial.println("The device started, now you can pair it with bluetooth!");
-    Serial.println("Device Name: HokienautsESP32");
-    Serial.print("BT MAC Address: ");
-    printDeviceAddress();
-    Serial.println("\n");
+
+  SerialBT.begin("ESP32test");
+  if(SerialBT){
+    Serial.println("Serial Bluetooth Started");
   }
   else{
-    Serial.println("Error starting Bluetooth");
+    Serial.println("Serial Bluetooth NOT Started");
   }
+  
+  delay(1000);
 }
+     
+void loop()
+{
+  if (Serial.available() > 0) {
+    serialInput = Serial.readStringUntil('\n') + "\n";
+    SerialBT.print(serialInput);
+  }
+  
+  
+  if (SerialBT.available()) {
+    btInput = SerialBT.readStringUntil('\n');
+    Serial.println(btInput);
 
-void loop() {
-  if (Serial.available()) { //send
-    SerialBT.write(Serial.read());
+    if(btInput == "Hello from PC"){
+      SerialBT.println("Hello from ESP32");
+    }
   }
-  if (SerialBT.available()) { //receive
-    Serial.println(SerialBT.read());
-  }
-  delay(20);
 }
