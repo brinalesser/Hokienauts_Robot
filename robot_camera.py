7e2c445f32e6e5fe3b3aa68fac2d10752d6d4af2
Sabrina Lesser <brinalesser@vt.edu>
2022-03-28 00:58:32 -0400

    fixed typo

--- robot_camera.py
+++ robot_camera.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def take_picture():
 
     t = datetime.now()
-    t_str = now.strftime('%H_%M_%S')
+    t_str = t.strftime('%H_%M_%S')
     image_name = 'image_'+t_str+'.png'
 
     cmd = 'raspistill -t 1000 -o '+image_name
